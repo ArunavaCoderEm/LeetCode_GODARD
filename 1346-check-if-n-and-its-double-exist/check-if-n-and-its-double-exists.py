ea5e2cc502a9e90f09b18dbd2f8f1a592a3d6707
Arunava Dutta <acecankill@gmail.com>
2024-01-15 23:02:45 +0530

    Delete 1346-check-if-n-and-its-double-exist/check-if-n-and-its-double-exists.py

--- 1346-check-if-n-and-its-double-exist/check-if-n-and-its-double-exists.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def checkIfExist(self, arr: List[int]) -> bool:
-        if (0 in arr and arr.count(0) > 1):
-            return True
-        for i in arr:
-            if(2*i in arr and arr.index(i) != arr.index(i*2)):
-                return True
-        return False
