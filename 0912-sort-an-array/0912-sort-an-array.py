6d4adeb4c0192d4c2229c66534dfe54cf08cc604
Arunava Dutta <acecankill@gmail.com>
2024-01-23 23:58:04 +0530

    Time: 510 ms (98.31%), Space: 23.7 MB (99.68%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        return nums
+# class Solution:
+#     def merge(arr,low,mid,high):
+#         i,j,k,A = low,mid+1,low,[]
+#     def sortArray(self, nums: List[int]) -> List[int]:
