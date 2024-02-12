9165eff92b5aa5e0c5de0dfa741929d3a4846495
Arunava Dutta <acecankill@gmail.com>
2024-02-12 21:45:34 +0530

    Time: 136 ms (61.11%), Space: 17.9 MB (83.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,4 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        c = []
-        num = list(set(nums))
-        for i in num:
-            c.append(nums.count(i))
-        for j in (num):
-            if (nums.count(j) == max(c)):
-                return j
-        
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        f = max(set(nums),key = nums.count)
+        return f
