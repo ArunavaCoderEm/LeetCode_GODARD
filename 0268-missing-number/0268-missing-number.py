249d636261c7f4f09deabeabbde453f3b055e267
Arunava Dutta <acecankill@gmail.com>
2024-02-20 21:37:12 +0530

    Time: 1503 ms (7.22%), Space: 17.8 MB (64.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,4 @@
-# class Solution(object):
-#     def missingNumber(self, nums):
-#         minn = min(nums)
-#         maxx = len(nums)
-#         for i in range (minn, maxx + 1):
-#             if (i not in nums):
-#                 return i
-#         return 0
-
-## THANKS TO HACKERRANK
-class Solution(object):
-    def missingNumber(self, nums):
-        summ = (len(nums)*(len(nums) + 1)) // 2;
-        return summ - sum(nums)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)+1):
+            if(i not in nums): return i
