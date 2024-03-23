ec9acec777402bdd3b4c96fcbeb759de4c81c77e
Arunava Dutta <acecankill@gmail.com>
2024-03-23 16:18:21 +0530

    Time: 120 ms (42.66%), Space: 23.4 MB (77.75%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        cita = sorted(citations,reverse = True)
+        res = 0
+        for i,j in enumerate(cita):
+            if(j >= (i + 1)): res = i + 1
+            else: break
+        return res
