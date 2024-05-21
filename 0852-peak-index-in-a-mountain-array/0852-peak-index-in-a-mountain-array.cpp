77c375808b03eba59278c0f8b941f889590855e9
Arunava Dutta <acecankill@gmail.com>
2024-05-21 22:21:02 +0530

    Update 0852-peak-index-in-a-mountain-array.cpp

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,9 +4,9 @@ public:
         int s = 0, e = arr.size() - 1;
         int mid = (s + (e - s) / 2);
         while(s <= e) {
-            // if peek
+            // if peak
             if(arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) return mid;
-            // if not peek
+            // if not peak
             else if(arr[mid] < arr[mid + 1]) s = mid + 1;
             else e = mid - 1;
             mid = (s + (e - s) / 2);
