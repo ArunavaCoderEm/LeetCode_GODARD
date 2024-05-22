6c623ee6ab36cf9a899f77b16c37844ae35538ae
Arunava Dutta <acecankill@gmail.com>
2024-05-22 20:52:25 +0530

    Time: 479 ms (31.54%), Space: 32.5 MB (81.72%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -8,6 +8,7 @@ def kindofdfs(s, count, ans, res):
             a.append(s[count : i+1])
             kindofdfs(s, i+1, a, res)
 ##### BRO WHY ONLY 60 % ?????? I CAN'T THINK OF A BETTER ONE !!!!!!!!!
+## OKAY NOW IT CAME 94% WITH SAME CODE !!! WTF
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
