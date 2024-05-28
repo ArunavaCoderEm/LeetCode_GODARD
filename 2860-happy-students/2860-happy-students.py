63e4248ad4ea3e8ceea2dfd88cdaa119cc0a916e
Arunava Dutta <acecankill@gmail.com>
2024-05-29 00:03:08 +0530

    Time: 458 ms (36.57%), Space: 28.3 MB (90.30%) - LeetHub

---
+++ 2860-happy-students/2860-happy-students.py
@@ -0,0 +1,12 @@
+class Solution:
+    # brute force 
+    def countWays(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 0
+        mx = max(nums)
+        if(mx < len(nums)): res += 1
+        if(nums[0] != 0 and mx < len(nums)): res += 1
+        for i in range(len(nums)):
+            if(i + 1 > nums[i] and i + 1 < nums[-1] and i + 1 < nums[i + 1]):
+                res += 1
+        return res
