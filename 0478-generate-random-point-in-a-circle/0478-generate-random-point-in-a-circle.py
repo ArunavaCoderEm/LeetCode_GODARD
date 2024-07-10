20ba9f78f521a10c2c65ebacdcd5b0e4571f2672
Godard <acecankill@gmail.com>
2024-07-10 22:22:22 +0530

    Time: 119 ms (27.11%), Space: 26.7 MB (21.48%) - LeetHub

---
+++ 0478-generate-random-point-in-a-circle/0478-generate-random-point-in-a-circle.py
@@ -0,0 +1,27 @@
+import random
+
+def generate_random_decimal(lower_bound, upper_bound, allow_negative=False):
+    if allow_negative and random.choice([True, False]):
+        return random.uniform(lower_bound, upper_bound) * -1
+    else:
+        return random.uniform(lower_bound, upper_bound)
+
+class Solution:
+
+    def __init__(self, radius: float, x_center: float, y_center: float):
+        self.r = radius
+        self.x = x_center
+        self.y = y_center
+
+    def randPoint(self) -> List[float]:
+        dist = self.r * math.sqrt(random.uniform(0, 1))
+        angle = random.uniform(0, 2 * math.pi)
+        x = self.x + dist * math.cos(angle)
+        y = self.y + dist * math.sin(angle)
+        return [x, y]
+
+# Example usage
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(radius, x_center, y_center)
+# param_1 = obj.randPoint()
