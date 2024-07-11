cbb6c0ad90382ed85303cf54e43d544619a9abb2
Godard <acecankill@gmail.com>
2024-07-11 18:06:57 +0530

    Time: 79 ms (57.93%), Space: 20.9 MB (9.87%) - LeetHub

---
+++ 0817-linked-list-components/0817-linked-list-components.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# dont particularly check for conseq but 
+# check each if next is not none then move forward the
+# connected component of LL
+## Ezz approach
+
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        
+        # with list only it was giving 56%
+        # convert to set as constant lookup
+        num_set = set(nums)
+        compo = 0
+        
+        if(len(nums) == 1): return 1
+        if(head == None): return None
+        if(head.next == None):
+            x = head.val
+            if(x in nums): return 1
+            
+        temp = head
+        
+        print(nums)
+        
+        while(temp != None):
+            if (temp.val in num_set and (temp.next is None or temp.next.val not in num_set)):
+                compo += 1
+            temp = temp.next
+        
+        return compo
